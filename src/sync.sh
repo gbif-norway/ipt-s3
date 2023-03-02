953febf8c1d602cbc51f5c2714562a27d42aad29
Michal Torma <michal.torma@nhm.uio.no>
2023-03-02 10:49:17 +0100

    Improve crontab creation and logging

--- src/sync.sh
+++ src/sync.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rsync -avzWS $IPT_DATA_DIR /root/s3data/ --delete-after --no-compress 
+rsync -avzWS $IPT_DATA_DIR /root/s3data/ --delete-after --no-compress
+echo $(date) "Backup complete" >> /var/log/backup.log
+echo $(date) "Backup complete"
