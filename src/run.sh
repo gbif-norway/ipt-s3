c549f418d31166c88893f09db9862730765df0f3
rukayaj@gmail.com <rukayaj@gmail.com>
2023-03-06 21:21:20 +0100

    Copy ojdbc8 driver in Dockerfile

--- src/run.sh
+++ src/run.sh
@@ -19,7 +19,7 @@ echo "Starting IPT!"
 catalina.sh run
 echo "IPT started, copying over ojdbc8 driver"
 sleep 25
-cp /srv/ipt/ojdbc8.jar /usr/local/tomcat/webapps/ROOT/WEB-INF/lib/ojdbc8.jar
+cp /root/ojdbc8.jar /usr/local/tomcat/webapps/ROOT/WEB-INF/lib/ojdbc8.jar
 rm /usr/local/tomcat/webapps/ROOT/WEB-INF/lib/ojdbc-14.jar
 echo "Soft restarting tomcat with ojdbc driver in place"
 touch /usr/local/tomcat/webapps/ROOT/WEB-INF/web.xml
