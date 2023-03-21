ba925ac8b68625ecaad6370268f32ff29f8103f8
rukayaj@gmail.com <rukayaj@gmail.com>
2023-03-21 17:03:31 +0100

    mc mirror should be set up before catalina.sh run

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
+echo "Set up bi-directional mirroring using mc mirror"
+mc alias set sigma2 https://storage.gbif-no.sigma2.no $S3_ACCESS_KEY $S3_SECRET_KEY
+mc mirror --overwrite --watch sigma2/ipt $IPT_DATA_DIR &> /dev/null
+mc mirror --overwrite --watch $IPT_DATA_DIR sigma2/ipt &> /dev/null
 echo "Starting IPT!"
 catalina.sh run & 
 echo "IPT started, copying over ojdbc8 driver"
-sleep 25
 cp /root/ojdbc8.jar /usr/local/tomcat/webapps/ROOT/WEB-INF/lib/ojdbc8.jar
 rm /usr/local/tomcat/webapps/ROOT/WEB-INF/lib/ojdbc-14.jar
 echo "Soft restarting tomcat with ojdbc driver in place"
 touch /usr/local/tomcat/webapps/ROOT/WEB-INF/web.xml
-mc alias set sigma2 https://storage.gbif-no.sigma2.no $S3_ACCESS_KEY $S3_SECRET_KEY
-mc mirror --overwrite --watch sigma2/ipt $IPT_DATA_DIR &> /dev/null
-mc mirror --overwrite --watch $IPT_DATA_DIR sigma2/ipt &> /dev/null
+sleep 25
 exec "$@"
