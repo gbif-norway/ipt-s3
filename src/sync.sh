71e141f4cd9e9deb2e7d53e898f62771510835e3
rukayaj@gmail.com <rukayaj@gmail.com>
2023-03-22 16:36:46 +0100

    Simplify

--- src/sync.sh
+++ src/sync.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-BUCKET="sigma2/ipt"
 LOG_FILE="/var/log/syncfail.log"
-
-function check_and_copy_bucket() {
-  if [ ! -d "$IPT_DATA_DIR" ]; then
-    echo "Directory $IPT_DATA_DIR does not exist, checking if the bucket is not empty."
-    bucket_size=$(mc ls "$BUCKET" | wc -l)
-    if [ "$bucket_size" -gt 0 ]; then
-      echo "Bucket is not empty, copying contents to $IPT_DATA_DIR."
-      mkdir -p "$IPT_DATA_DIR"
-      mc cp --recursive "$BUCKET" "$IPT_DATA_DIR"
-    fi
-  else
-    dir_size=$(find "$IPT_DATA_DIR" -mindepth 1 -not -name "lost+found" | wc -l)
-    if [ "$dir_size" -eq 0 ]; then
-      echo "Directory $IPT_DATA_DIR is empty, copying contents from the bucket."
-      mc cp --recursive "$BUCKET" "$IPT_DATA_DIR/.."
-    fi
-  fi
-}
 
 function run_command() {
   local cmd="$1"
@@ -32,7 +13,9 @@ function run_command() {
   done
 }
 
-check_and_copy_bucket
+echo "Initial sync. If a file exists in $IPT_DATA_DIR/.. and it's different on the sigma2/ipt bucket, we overwrite what's on the bucket. This may possibly cause some problems with the corema datasets, where the bucket should take precedence."
+mc mirror --overwrite $IPT_DATA_DIR/.. sigma2/ipt
 
+echo "Start continuous bi-directional mirroring"
 run_command "mc mirror --overwrite --watch sigma2/ipt $IPT_DATA_DIR/.. &> /dev/null" &
 run_command "mc mirror --overwrite --watch --remove $IPT_DATA_DIR/.. sigma2/ipt &> /dev/null" &
