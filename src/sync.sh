f8a34b063151de2d3fe33b94f34253673e60b638
Michal Torma <michal.torma@nhm.uio.no>
2023-03-22 13:07:35 +0100

    Implement sync script
    This commit introduces a bash script that performs the following tasks:
    - Checks if the IPT_DATA_DIR directory exists and if the sigma2/ipt bucket is not empty.
    - If the directory doesn't exist and the bucket is not empty, creates the directory and copies the contents of the bucket to IPT_DATA_DIR.
    - Continuously runs two mc mirror commands in the background, watching for changes and mirroring data between IPT_DATA_DIR and sigma2/ipt.
    - If either mc mirror command fails, it will restart after a 5-minute delay.
    - Logs the failures along with their exit codes and timestamps to /var/log/syncfail.log.

--- src/sync.sh
+++ src/sync.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+BUCKET="sigma2/ipt"
 LOG_FILE="/var/log/syncfail.log"
+
+function check_and_copy_bucket() {
+  if [ ! -d "$IPT_DATA_DIR" ]; then
+    echo "Directory $IPT_DATA_DIR does not exist, checking if the bucket is not empty."
+    bucket_size=$(mc ls "$BUCKET" | wc -l)
+    if [ "$bucket_size" -gt 0 ]; then
+      echo "Bucket is not empty, copying contents to $IPT_DATA_DIR."
+      mkdir -p "$IPT_DATA_DIR"
+      mc cp --recursive "$BUCKET" "$IPT_DATA_DIR"
+    fi
+  fi
+}
 
 function run_command() {
   local cmd="$1"
@@ -12,6 +25,8 @@ function run_command() {
     sleep 300
   done
 }
+
+check_and_copy_bucket
 
 run_command "mc mirror --overwrite --watch sigma2/ipt $IPT_DATA_DIR &> /dev/null" &
 run_command "mc mirror --overwrite --watch $IPT_DATA_DIR sigma2/ipt &> /dev/null" &
