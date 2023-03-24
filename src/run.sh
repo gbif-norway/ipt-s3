c28ad618795a253e2f6d002ff622c147cfa830be
Michal Torma <michal.torma@nhm.uio.no>
2023-03-24 15:46:26 +0100

    Implement rclone syncing every hour

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
-echo "Set up bi-directional mirroring using mc mirror"
-mc alias set sigma2 https://storage.gbif-no.sigma2.no $S3_ACCESS_KEY $S3_SECRET_KEY
+echo "Before startup, sync from remote to local"
+rclone sync --update --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ sigma2:$S3_BUCKET_NAME $IPT_DATA_DIR
+echo "Setup cron sync"
 chmod a+x /root/sync.sh
 /root/sync.sh
+printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/sync-cron
+crontab /etc/cron.d/sync-cron
+cron
 echo "Starting IPT!"
 catalina.sh run &
 tomcat_pid=$!
--- src/sync.sh
+++ src/sync.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-LOG_FILE="/var/log/syncfail.log"
 
-function run_command() {
-  local cmd="$1"
-  while true; do
-    echo "Starting command: $cmd"
-    $cmd
-    local exit_code=$?
-    echo "Command exited with code $exit_code. Restarting in 5 minutes..."
-    echo "$(date) - Command \"$cmd\" exited with code $exit_code." >> $LOG_FILE
-    sleep 300
-  done
-}
-
-echo "Initial sync. If a file exists in $IPT_DATA_DIR/.. and it's different on the sigma2/ipt bucket, we overwrite what's on the bucket. This may possibly cause some problems with the corema datasets, where the bucket should take precedence."
-mc mirror --overwrite $IPT_DATA_DIR/.. sigma2/ipt
-
-echo "Start continuous bi-directional mirroring"
-run_command "mc mirror --overwrite --watch sigma2/ipt $IPT_DATA_DIR/.. &> /dev/null" &
-run_command "mc mirror --overwrite --watch --remove $IPT_DATA_DIR/.. sigma2/ipt &> /dev/null" &
+echo "$(date -I) Sync starting" >> /var/log/sync.log
+# Sync from local to remote and if sucessful, remote to local (only newer files)
+rclone sync --update --delete-during --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ $IPT_DATA_DIR sigma2:$S3_BUCKET_NAME && \
+rclone sync --update --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ sigma2:$S3_BUCKET_NAME $IPT_DATA_DIR && \
+echo "$(date -I) Sync complete succesfully" >> /var/log/sync.log
