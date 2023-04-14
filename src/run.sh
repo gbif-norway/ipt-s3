f915da42ae70b1d48eff91edc5ce26467eeaa22c
Rukaya <rukayaj@gmail.com>
2023-04-14 15:21:28 +0200

    Remove all syncing

--- src/run.sh
+++ src/run.sh
@@ -3,11 +3,11 @@
 # Run this manually in the container only once if the container is started up with an empty pvc
 # Otherwise, remote should only be used to back up
 # rclone sync --update --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ sigma2:$S3_BUCKET_NAME $IPT_DATA_DIR
-echo "Setup cron sync (from local to remote, for backups), to run every hour"
-chmod a+x /root/sync.sh
-printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/sync-cron
-crontab /etc/cron.d/sync-cron
-cron
+#echo "Setup cron sync (from local to remote, for backups), to run every hour"
+#chmod a+x /root/sync.sh
+#printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/sync-cron
+#crontab /etc/cron.d/sync-cron
+#cron
 echo "Starting IPT!"
 catalina.sh run &
 tomcat_pid=$!
