9be7a2ee1a2087e0e0e45d381b01d1118775e96a
Michal Torma <michal.torma@nhm.uio.no>
2023-06-06 13:06:07 +0200

    Cleanup and sync improvement

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# The rclone sync below syncs from remote to local
-# Run this manually in the container only once if the container is started up with an empty pvc
-# Otherwise, remote should only be used to back up
-# rclone sync --update --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ sigma2:$S3_BUCKET_NAME $IPT_DATA_DIR
-#echo "Setup cron sync (from local to remote, for backups), to run every hour"
-#chmod a+x /root/sync.sh
-#printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/sync-cron
-#crontab /etc/cron.d/sync-cron
-#cron
+echo "Create the bucket if it doesn't exist"
+set +e
+s4cmd mb s3://$S3_BUCKET_NAME --endpoint-url $S3_HOST
+set -e
+
+echo "Setup cron sync (from local to remote, for backups), to run every hour"
+chmod a+x /root/sync.sh
+printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/sync-cron
+crontab /etc/cron.d/sync-cron
+cron
+
 echo "Starting IPT!"
 catalina.sh run &
 tomcat_pid=$!
--- src/sync.sh
+++ src/sync.sh
@@ -2,5 +2,6 @@
 
 echo "$(date -I) Sync starting" >> /var/log/sync.log
 # Sync from local to remote for backups
-rclone sync --update --delete-during --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ $IPT_DATA_DIR sigma2:$S3_BUCKET_NAME && \
-echo "$(date -I) Sync complete succesfully" >> /var/log/sync.log
+# rclone sync --update --delete-during --verbose --transfers 10 --checkers 10 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .~tmp~/ $IPT_DATA_DIR sigma2:$S3_BUCKET_NAME && \
+s4cmd dsync --recursive --verbose --sync-check --num-threads=5 $IPT_DATA_DIR s3://$S3_BUCKET_NAME --endpoint-url $S3_HOST && \
+echo "$(date -I'seconds') Sync complete succesfully" >> /var/log/sync.log
