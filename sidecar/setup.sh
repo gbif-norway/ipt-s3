a43353b4b760ab4fc15dce6c017dbe0e10fe287c
rukayaj@gmail.com <rukayaj@gmail.com>
2023-06-09 11:40:40 +0200

    This also seems to work to set the env vars and it's better

--- sidecar/setup.sh
+++ sidecar/setup.sh
@@ -4,16 +4,19 @@ set +e
 s4cmd mb s3://$S3_BUCKET_NAME --endpoint-url $S3_HOST
 set -e
 
+# Set environment variables so they are accessible to cron
+env >> /etc/environment
+
 echo "Setup cron sync (from local to remote, for backups) every hour"
 chmod a+x /root/sync.sh
-printf '%s\n\n' '1 * * * * S3_BUCKET_NAME=$S3_BUCKET_NAME S3_HOST=$S3_HOST S3_SECRET_KEY=$S3_SECRET_KEY S3_ACCESS_KEY=$S3_ACCESS_KEY /root/sync.sh 2>&1' > /etc/cron.d/cron-jobs
+printf '%s\n\n' '1 * * * * /root/sync.sh 2>&1' > /etc/cron.d/cron-jobs
 
 if [ -z "$S3_ZIP_BUCKET_NAME" ]; then
     echo "$(date): S3_ZIP_BUCKET_NAME is not set, and DWCAs will not be unzipped to this IPT" >> /var/log/unzip.log
 else
     echo "Setup cron unzip (from remote, download dwca and unzip to local) every Tuesday at 2pm"
     chmod a+x /root/unzip.sh
-    printf '%s\n\n' '0 14 * * 2 S3_ZIP_BUCKET_NAME=$S3_ZIP_BUCKET_NAME S3_BUCKET_NAME=$S3_BUCKET_NAME S3_HOST=$S3_HOST S3_SECRET_KEY=$S3_SECRET_KEY S3_ACCESS_KEY=$S3_ACCESS_KEY /root/unzip.sh 2>&1' >> /etc/cron.d/cron-jobs
+    printf '%s\n\n' '0 14 * * 2 /root/unzip.sh 2>&1' >> /etc/cron.d/cron-jobs
 fi
 
 crontab /etc/cron.d/cron-jobs
